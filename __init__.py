bd8bbc383bc39d182cd13f9d5079c8cbef5bd4c1
David Bell <dave@evad.info>
2016-02-22 16:57:59 +0000

    Release: 1.3.2. Fixes #58 Fixes #59 Fixes #60

--- __init__.py
+++ __init__.py
@@ -90,7 +90,7 @@ DISABLE_APP=True
 THEME_DEFAULT='lumen'
 
 ## Bargate internal version number
-VERSION='1.3.1'
+VERSION='1.3.2'
 
 ## Flask defaults (changed to what we prefer)
 SESSION_COOKIE_SECURE      = True
