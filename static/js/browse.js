3cef775d120756d9ae51f6400a04c4ff58fbce34
David Bell <dave@evad.info>
2016-03-26 23:37:03 +0000

    removed legacy files, list view sorting now works...ish

--- static/js/browse.js
+++ static/js/browse.js
@@ -127,25 +127,31 @@ $(document).ready(function()
 	/* sort entries in a directory */
 	$('.dir-sortby-name').on( 'click', function()
 	{
-		$container.isotope({ sortBy: 'name' });
+		/*$container.isotope({ sortBy: 'name' });*/
+		$('#dir').DataTable().order([3,'asc']).draw();
+
 		$('.sortby-check').addClass('invisible');
 		$('.dir-sortby-name span').removeClass('invisible');
 	});
-	$('.dir-sortby-type').on( 'click', function()
-	{
-		$container.isotope({ sortBy: 'type' });
-		$('.sortby-check').addClass('invisible');
-		$('.dir-sortby-type span').removeClass('invisible');
-	});
 	$('.dir-sortby-mtime').on( 'click', function()
 	{
-		$container.isotope({ sortBy: 'mtime' });
+		/*$container.isotope({ sortBy: 'mtime' });*/
+		$('#dir').DataTable().order([4,'asc']).draw();
 		$('.sortby-check').addClass('invisible');
 		$('.dir-sortby-mtime span').removeClass('invisible');
 	});
+	$('.dir-sortby-type').on( 'click', function()
+	{
+		/*$container.isotope({ sortBy: 'type' });*/
+		$('#dir').DataTable().order([5,'asc']).draw();
+
+		$('.sortby-check').addClass('invisible');
+		$('.dir-sortby-type span').removeClass('invisible');
+	});
 	$('.dir-sortby-size').on( 'click', function()
 	{
-		$container.isotope({ sortBy: 'size' });
+		/*$container.isotope({ sortBy: 'size' });*/
+		$('#dir').DataTable().order([6,'asc']).draw();
 		$('.sortby-check').addClass('invisible');
 		$('.dir-sortby-size span').removeClass('invisible');
 	});
